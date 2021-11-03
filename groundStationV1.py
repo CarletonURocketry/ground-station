013b7db3c9e5afa9f8774d0f9fc05e1e9bcf9ee7
Zacchaeus Leung <zakthertemsdev@outlook.com>
2021-11-02 22:16:21 -0400

    completely init radio

--- groundStationV1.py
+++ groundStationV1.py
@@ -8,16 +8,28 @@ def init_ground_station():
 
     # initlize a serial port  
     ser = serial.Serial(
-                    ser.baudrate=57600
+                baudrate=57600,
                     # number of bits per bytes ((configure packet size?)
-                    ser.bytesize=serial.EIGHTBITS
-                    ser.parity=serial.PARITY_NONE  # set parity check: no parity
-                    ser.stopbits=0  # number of stop bits
-                    ser.rtscts=False  # disable hardware (RTS/CTS) flow control
-                    ser.
+                bytesize=serial.EIGHTBITS,
+                parity=serial.PARITY_NONE,  # set parity check: no parity
+                stopbits=0,  # number of stop bits
+                rtscts=False,  # disable hardware (RTS/CTS) flow control
                     )  
    #set the frequency of the radio 
-    ser.write(bytes(str("radio set freq 433000000") + "\r\n", "utf-8"))
+    ser.write(bytes(str("radio set freq 433050000") + "\r\n", "utf-8"))
+    ser.write(bytes(str("radio set pwr 14 ") + "\r\n", "utf-8"))
+    ser.write(bytes(str("radio set sf sf9 ") + "\r\n", "utf-8"))
+    ser.write(bytes(str("radio set cr 4/7 ") + "\r\n", "utf-8"))
+    ser.write(bytes(str("radio set bw 500 ") + "\r\n", "utf-8"))
+    ser.write(bytes(str("radio set pr 6 ") + "\r\n", "utf-8"))
+    ser.write(bytes(str("radio set crc on  ") + "\r\n", "utf-8"))
+    ser.write(bytes(str("radio set  iqi off  ") + "\r\n", "utf-8"))
+    ser.write(bytes(str("radio set  sync 43  ") + "\r\n", "utf-8"))`
+
+
+
+    
+
 
 
 
