699d7d1e39b8c137100b352dea539935998048f9
Matteo Golin <matteo.golin@gmail.com>
2022-10-07 13:48:49 -0400

    Fixed format strings

--- packets.py
+++ packets.py
@@ -1,56 +1,56 @@
 from random import randrange
 
 hex_bin_dic = {
-              '0': '0000',
-              '1': '0001',
-              '2': '0010',
-              '3': '0011',
-              '4': '0100',
-              '5': '0101',
-              '6': '0110',
-              '7': '0111',
-              '8': '1000',
-              '9': '1001',
-              'A': '1010',
-              'B': '1011',
-              'C': '1100',
-              'D': '1101',
-              'E': '1110',
-              'F': '1111',
-              'a': '1010',
-              'b': '1011',
-              'c': '1100',
-              'd': '1101',
-              'e': '1110',
-              'f': '1111'
-              }
+    '0': '0000',
+    '1': '0001',
+    '2': '0010',
+    '3': '0011',
+    '4': '0100',
+    '5': '0101',
+    '6': '0110',
+    '7': '0111',
+    '8': '1000',
+    '9': '1001',
+    'A': '1010',
+    'B': '1011',
+    'C': '1100',
+    'D': '1101',
+    'E': '1110',
+    'F': '1111',
+    'a': '1010',
+    'b': '1011',
+    'c': '1100',
+    'd': '1101',
+    'e': '1110',
+    'f': '1111'
+}
 
 bin_hex_dic = {v: k for k, v in hex_bin_dic.items()}
 
 kx134_1211_dic = {
-              '0x0': 0.781,
-              '0x1': 1.563,
-              '0x2': 3.125,
-              '0x3': 6.25,
-              '0x4': 12.5,
-              '0x5': 25,
-              '0x6': 50,
-              '0x7': 100,
-              '0x8': 200,
-              '0x9': 400,
-              '0xA': 800,
-              '0xB': 1600,
-              '0xC': 3200,
-              '0xD': 6400,
-              '0xE': 12800,
-              '0xF': 25600,
-              '0xa': 800,
-              '0xb': 1600,
-              '0xc': 3200,
-              '0xd': 6400,
-              '0xe': 12800,
-              '0xf': 25600,
-              }
+    '0x0': 0.781,
+    '0x1': 1.563,
+    '0x2': 3.125,
+    '0x3': 6.25,
+    '0x4': 12.5,
+    '0x5': 25,
+    '0x6': 50,
+    '0x7': 100,
+    '0x8': 200,
+    '0x9': 400,
+    '0xA': 800,
+    '0xB': 1600,
+    '0xC': 3200,
+    '0xD': 6400,
+    '0xE': 12800,
+    '0xF': 25600,
+    '0xa': 800,
+    '0xb': 1600,
+    '0xc': 3200,
+    '0xd': 6400,
+    '0xe': 12800,
+    '0xf': 25600,
+}
 
 
 def hex_str_to_int(hex_string):
@@ -58,7 +58,6 @@ def hex_str_to_int(hex_string):
 
 
 def bin_to_hex(bin_string):
-
     return hex(int(bin_string, 2))
 
 
@@ -78,7 +77,7 @@ def hex_to_bin(hex_string):
     return ret_string
 
 
-def signed_hex_str_to_int(hex_string,):
+def signed_hex_str_to_int(hex_string, ):
     # this function preserves number of bits
     bin_input = hex_to_bin(hex_string)
     if bin_input[0] == '1':
@@ -88,7 +87,6 @@ def signed_hex_str_to_int(hex_string,):
 
 
 def signed_bin_str_to_int(bin_string):
-
     return signed_hex_str_to_int(hex(int(bin_string, 2)))
 
 
@@ -107,10 +105,10 @@ class AltitudeData:
         self.set_altitude(raw)
 
     def __str__(self):
-        return("time:{}\npressure:{}\ntemperature:{}\naltitude:{}\n".format(self.time,
-                                                                  self.pressure,
-                                                                  self.temperature,
-                                                                  self.altitude))
+        return f"time:{self.time}\n" \
+               f"pressure:{self.pressure}\n" \
+               f"temperature:{self.temperature}\n" \
+               f"altitude:{self.altitude}\n"
 
     def set_time(self, raw):
         """
@@ -122,21 +120,18 @@ class AltitudeData:
         self.time = hex_str_to_int(raw[0:8])
 
     def set_pressure(self, raw):
-
         # pressure in kilo pascals
         pressure = hex_str_to_int(raw[8:16]) / 1000
 
         self.pressure = pressure
 
     def set_temp(self, raw):
-
         # temperature in recieved in millidegree celcius
         temperature = hex_str_to_int(raw[16:24]) / 1000
 
         self.temperature = temperature
 
     def set_altitude(self, raw):
-
         # altitude in mm
         altitude_mm = hex_str_to_int(raw[24:32])
 
@@ -147,7 +142,6 @@ class AltitudeData:
 class AccelerationData:
 
     def __init__(self, raw, resolution):
-
         self.time_stamp = None
         self.fsr = None
         self.x_axis = None
@@ -161,11 +155,11 @@ class AccelerationData:
         self.set_z_axis(raw, resolution)
 
     def __str__(self):
-        return('{}\n{}\n{}\n{}\n{}\n'.format(self.time_stamp,
-                                             self.fsr,
-                                             self.x_axis,
-                                             self.y_axis,
-                                             self.z_axis))
+        return f"{self.time_stamp}\n" \
+               f"{self.fsr}\n" \
+               f"{self.x_axis}\n" \
+               f"{self.y_axis}\n" \
+               f"{self.z_axis}\n"
 
     def set_time(self, raw):
         """
@@ -182,7 +176,7 @@ class AccelerationData:
         self.fsr = (fsr_unadjusted // (2 ** adjustment_factor))
 
     def set_x_axis(self, raw, resolution):
-        #todo: these are signed values and so must be fixed
+        # todo: these are signed values and so must be fixed
         measurement = hex_str_to_int(raw[12:16])
         self.x_axis = measurement * (self.fsr / (2 ** 15))
 
@@ -198,7 +192,6 @@ class AccelerationData:
 class AngularVelocityData:
 
     def __init__(self, raw, resolution):
-
         self.time_stamp = None
         self.fsr = None
         self.x_velocity = None
@@ -234,16 +227,16 @@ class AngularVelocityData:
         self.z_velocity = measurement * (self.fsr / (2 ** 15))
 
     def __str__(self):
-        return('fsr: {}\nx_velocity: {}\ny_velocity: {}\nz_velocity: {}\n'.format(self.fsr,
-                                                                                  self.x_velocity,
-                                                                                  self.y_velocity,
-                                                                                  self.z_velocity))
+        return f"fsr: {self.fsr}\n" \
+               f"x_velocity: {self.x_velocity}\n" \
+               f"y_velocity: {self.y_velocity}\n" \
+               f"z_velocity: {self.z_velocity}\n"
 
 
 class GNSSMetaData:
-
     class Info:
         """stores metadata on satellites used in the GNSS"""
+
         def __init__(self, raw):
 
             # elevation of GNSS satellite in degrees
@@ -280,11 +273,11 @@ class GNSSMetaData:
 
         def __str__(self):
 
-            return ('elevation: {}\n'
-                    'SNR: {}\n'
-                    'ID: {}\n'
-                    'Azimuth: {}\n'
-                    'type: {}\n'.format(self.elevation, self.SNR, self.ID, self.azimuth, self.type))
+            return f"elevation: {self.elevation}\n" \
+                    f"SNR: {self.SNR}\n" \
+                    f"ID: {self.ID}\n" \
+                    f"Azimuth: {self.azimuth}\n" \
+                    f"type: {self.type}\n"
 
     def __init__(self, raw):
         self.mission_time = None
@@ -318,7 +311,7 @@ class GNSSMetaData:
 
         for i in range(num_sats):
             # each satellite's info is stored in 32 bits
-            meta_data = self.Info(sat_info[i*32: i*32 + 32])
+            meta_data = self.Info(sat_info[i * 32: i * 32 + 32])
             self.sat_info[meta_data.ID] = meta_data
 
 
@@ -397,29 +390,18 @@ class GNSSLocationData:
             self.fix_type = 'error'
 
     def __str__(self):
-        return('fix time: {}\n'
-               'latitude: {}\n'
-               'longitude: {}\n'
-               'utc_time: {}\n'
-               'altitude: {}\n'
-               'rocket speed: {}\n'
-               'rocket_course: {}\n'
-               'pdop: {}\n'
-               'hdop: {}\n'
-               'vdop: {}\n'
-               'num_sats: {}\n'
-               'fix_type: {}\n'.format(self.fix_time,
-                                       self.latitude,
-                                       self.longitude,
-                                       self.utc_time,
-                                       self.altitude,
-                                       self.rocket_speed,
-                                       self.rocket_course,
-                                       self.pdop,
-                                       self.hdop,
-                                       self.vdop,
-                                       self.num_sats,
-                                       self.fix_type))
+        return f"fix time: {self.fix_time}\n" \
+                f"latitude: {self.latitude}\n" \
+                f"longitude: {self.longitude}\n" \
+                f"utc_time: {self.utc_time}\n" \
+                f"altitude: {self.altitude}\n" \
+                f"rocket speed: {self.rocket_speed}\n" \
+                f"rocket_course: {self.rocket_course}\n" \
+                f"pdop: {self.pdop}\n" \
+                f"hdop: {self.hdop}\n" \
+                f"vdop: {self.vdop}\n" \
+                f"num_sats: {self.num_sats}\n" \
+                f"fix_type: {self.fix_type}\n"
 
 
 class MPU9250Data:
@@ -428,7 +410,6 @@ class MPU9250Data:
     class Measurement:
 
         def __init__(self, raw_bin):
-
             print(raw_bin)
             self.time_stamp = int(raw_bin[0:32], 2)
             self.accel_x = int(raw_bin[32:48], 2)
@@ -448,31 +429,19 @@ class MPU9250Data:
             self.mag_resolution = int(raw_bin[196], 2)
 
         def __str__(self):
-            return "time: {}\n" \
-                   "temperature: {}\n" \
-                   "accel x: {}\n" \
-                   "accel y: {}\n" \
-                   "accel z: {}\n" \
-                   "gyro x: {}\n" \
-                   "gyro y: {}\n" \
-                   "gyro z: {}\n" \
-                   "mag x: {}\n" \
-                   "mag y: {}\n" \
-                   "mag z: {}\n" \
-                   "mag valid: {}\n" \
-                   "mag resolution: {}\n".format(self.time_stamp,
-                                                 self.temperature,
-                                                 self.accel_x,
-                                                 self.accel_y,
-                                                 self.accel_z,
-                                                 self.gyro_x,
-                                                 self.gyro_y,
-                                                 self.gyro_z,
-                                                 self.mag_x,
-                                                 self.mag_y,
-                                                 self.mag_z,
-                                                 self.mag_valid,
-                                                 self.mag_resolution)
+            return f"time: {self.time_stamp}\n" \
+                   f"temperature: {self.temperature}\n" \
+                   f"accel x: {self.accel_x}\n" \
+                   f"accel y: {self.accel_y}\n" \
+                   f"accel z: {self.accel_z}\n" \
+                   f"gyro x: {self.gyro_x}\n" \
+                   f"gyro y: {self.gyro_y}\n" \
+                   f"gyro z: {self.gyro_z}\n" \
+                   f"mag x: {self.mag_x}\n" \
+                   f"mag y: {self.mag_y}\n" \
+                   f"mag z: {self.mag_z}\n" \
+                   f"mag valid: {self.mag_valid}\n" \
+                   f"mag resolution: {self.mag_resolution}\n"
 
     def __init__(self, raw):
         self.time_stamp = None
@@ -503,13 +472,12 @@ class MPU9250Data:
 
         # get the fsr of the accelerometer
         raw_a_fsr = int(raw_bin[39:41], 2)
-        self.accelerometer_fsr = 2** (raw_a_fsr + 1)
+        self.accelerometer_fsr = 2 ** (raw_a_fsr + 1)
 
-        #TODO: this needs to be finished after we recieve an input from this device
+        # TODO: this needs to be finished after we receive an input from this device
 
 
 class KX1341211Data:
-
     class Measurement:
         def __init__(self, raw):
             self.x_accel = None
@@ -532,11 +500,9 @@ class KX1341211Data:
                 self.z_accel = int(raw[32:48], 2)
 
         def __str__(self):
-            return ('x: {}\n'
-                    'y: {}\n'
-                    'z: {}\n'.format(self.x_accel,
-                                     self.y_accel,
-                                     self.z_accel))
+            return f"x: {self.x_accel}\n" \
+                   f"y: {self.y_accel}\n" \
+                   f"z: {self.z_accel}\n"
 
     def __init__(self, raw):
 
@@ -563,17 +529,11 @@ class KX1341211Data:
             data_points_str += str(m)
             data_points_str += '____\n'
 
-        return('time stamp: {}\n'
-               'data rate: {}\n'
-               'full scale range: {}\n'
-               'corner frequency: {}\n'
-                'measurements:\n{}'
-               '______________________'.format(self.time_stamp,
-                                               self.data_rate,
-                                               self.full_scale_range,
-                                               self.corner_freq,
-                                               data_points_str)
-               )
+        return f"time stamp: {self.time_stamp}\n" \
+               f"data rate: {self.data_rate}\n" \
+               f"full scale range: {self.full_scale_range}\n" \
+               f"corner frequency: {self.corner_freq}\n" \
+               f"measurements:\n{data_points_str}" + "-" * 20
 
     def setup(self, raw):
 
