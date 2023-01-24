bcfded759a24ea14063de3e92dd0a707fcd3f7bb
linguini1 <matteo.golin@gmail.com>
2023-01-24 10:49:28 -0500

    Added some minor docstrings and typign to modules in the misc folder.

--- modules/misc/converter.py
+++ modules/misc/converter.py
@@ -1,6 +1,16 @@
+# Unit conversion functions
+# Thomas Selwyn (Devil)
+# Matteo Golin (linguini1)
+
 def celsius_to_fahrenheit(celsius: float) -> float:
+
+    """Returns the passed celsius value in fahrenheit as a float."""
+
     return round((celsius * 1.8) + 32, 1)
 
 
 def metres_to_feet(metres: float) -> float:
+
+    """Returns the passed metres value in feet as a float."""
+
     return round(metres * 3.28, 1)
