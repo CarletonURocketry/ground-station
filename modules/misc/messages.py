787b4d86a2af5b179b3e91fc430a3b137ae3868b
linguini1 <matteo.golin@gmail.com>
2023-05-31 10:54:38 -0400

    Updated ASCII art to be part of an external file for easier changes.

--- modules/misc/messages.py
+++ modules/misc/messages.py
@@ -1,29 +1,33 @@
-# Display for the console window
+# Displays terminal art for server launch
 # Thomas Selwyn (Devil)
+# Matteo Golin
+
+# Imports
+import datetime as dt
+
+# Constants
+ART_FILE: str = "launch.txt"
+FIELDS: dict[str, str] = {"rocket name": "ROCKET_NAME", "version": "VERSION", "date": "DATE"}
+
+
+def load_art() -> str:
+    """Returns the launch screen ASCII art as a string."""
+    with open(ART_FILE, "r") as file:
+        art = file.read()
+    return art
+
+
+def populate_fields(art: str, rocket_name: str, version: str) -> str:
+    """Returns the launch screen ASCII art with its fields populated as a string."""
+
+    art = art.replace(FIELDS["rocket name"], rocket_name)
+    art = art.replace(FIELDS["version"], version)
+    art = art.replace(FIELDS["date"], dt.date.today().strftime("%d %m, %Y"))
+
 
 def print_cu_rocket(rocket_name: str, version: str) -> None:
-
     """Prints the CUInSpace rocket ASCII art with information about the rocket and software version."""
 
-    print(fr"""
-           ^
-          / \
-         /___\
-        |=   =|              |\___/|
-        |     |             /       \
-        | C U |             |    /\__/|
-        | I N |             ||\  <.><.>
-        |Space|             | _     > )
-        |     |             \   /----
-        |     |              |   -\/
-       /|##!##|\             /     \
-      / |##!##| \
-     /  |##!##|  \
-    |  / ^ | ^ \  |
-    | /  ( | )  \ |
-    |/   ( | )   \|
-        ((   ))
-       ((  :  ))            CU InSpace Avionics Telemetry Server
-       ((  :  ))            {f"Rocket": <11}{rocket_name}
-        ((   ))             {f"Version": <11}{version}
-          ( )               {f"Author": <11}{'Thomas Selwyn (Devil)'}""")
+    art = load_art()
+    art = populate_fields(art, rocket_name, version)
+    print(art)
