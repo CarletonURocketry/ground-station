4b1e8be996237b78a7c9051a9be6a2d1dc6e07b0
linguini1 <matteo.golin@gmail.com>
2023-06-15 16:18:19 -0400

    Resolved type errors in cli.py

--- modules/misc/cli.py
+++ modules/misc/cli.py
@@ -11,25 +11,25 @@ DESC: str = "Select some starting parameters for the telemetry server."
 
 # Custom types
 def file_path(path: str):
-    """Verifies that the argument is a valid filepath."""
+    """Verifies that the argument is a valid file path."""
 
     if os.path.isfile(path) or os.access(os.path.dirname(path), os.W_OK):
         return path
     else:
-        raise FileNotFoundError(f"{path} is not a valid filepath.")
+        raise FileNotFoundError(f"{path} is not a valid file path.")
 
 
 # Arguments
 parser = argparse.ArgumentParser(description=DESC)
 
-parser.add_argument(
+_ = parser.add_argument(
     "-l",
     help="Selects the logging level for messages in the console.",
     choices=["debug", "info", "warning", "error", "critical"],
-    default="info"
+    default="info",
 )
 
-parser.add_argument(
+_ = parser.add_argument(
     "-o",
     help="Output file for logging messages. Logs to console by default.",
     type=file_path,
