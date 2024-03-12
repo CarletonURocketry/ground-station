d82d717386f2701f109876bc9ddca9abb549359e
Elias Hawa <elias.hawa2468@gmail.com>
2024-03-12 18:44:32 -0400

    Update block header tests

--- tests/parsing/test_block_header.py
+++ tests/parsing/test_block_header.py
@@ -2,7 +2,7 @@
 
 # Imports
 import pytest
-from modules.telemetry.block import BlockHeader
+from modules.telemetry.v1.block import BlockHeader
 
 
 # Fixtures
@@ -10,69 +10,66 @@ from modules.telemetry.block import BlockHeader
 def header1() -> str:
     """
     Data block header with the following attributes:
-    length: 20 bytes
-    cryptographic signature: false
-    message type: 2
-    message sub type: 3
+    length: 12 bytes
+    message type: 0
+    message sub type: 1
     destination address: 0
     """
-    return "840C0000"
+    return "02000100"
 
 
 @pytest.fixture
 def header2() -> str:
     """
     Data block header with the following attributes:
-    length: 8 bytes
-    cryptographic signature: false
+    length: 12 bytes
     message type: 0
-    message sub type: 0
-    destination address: 0xF
+    message sub type: 2
+    destination address: 0
     """
-    return "01000F20"
+    return "02000200"
 
 
 @pytest.fixture
 def header3() -> str:
     """
     Data block header with the following attributes:
-    length: 20 bytes
-    cryptographic signature: false
-    message type: 2
-    message sub type: 1
+    length: 12 bytes
+    message type: 0
+    message sub type: 3
     destination address: 0
     """
-    return "84040000"
+    return "02000300"
 
 
 def test_parsing_header1(header1: str):
     """Ensure that parsing a block header works as expected."""
     hdr = BlockHeader.from_hex(header1)
 
-    assert len(hdr) == 20
-    assert hdr.has_crypto is False
-    assert hdr.message_type == 2
-    assert hdr.message_subtype == 3
+    assert len(hdr) == 12
+    assert hdr.message_type == 0
+    assert hdr.message_subtype == 1
     assert hdr.destination == 0
+    assert hdr.valid == True
 
 
 def test_parsing_header2(header2: str):
     """Ensure that parsing a block header works as expected."""
     hdr = BlockHeader.from_hex(header2)
 
-    assert len(hdr) == 8
-    assert hdr.has_crypto == 0
+    assert len(hdr) == 12
     assert hdr.message_type == 0
-    assert hdr.message_subtype == 0
-    assert hdr.destination == 0xF
+    assert hdr.message_subtype == 2
+    assert hdr.destination == 0
+    assert hdr.valid == True
 
 
 def test_parsing_header3(header3: str):
     """Ensure that parsing a block header works as expected."""
     hdr = BlockHeader.from_hex(header3)
 
-    assert len(hdr) == 20
-    assert hdr.has_crypto == 0
-    assert hdr.message_type == 2
-    assert hdr.message_subtype == 1
+    assert len(hdr) == 12
+    assert hdr.message_type == 0
+    assert hdr.message_subtype == 3
     assert hdr.destination == 0
+    assert hdr.valid == True
