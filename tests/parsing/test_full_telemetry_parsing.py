94178d1f8b0cb8ba8d59e5c513aa06cfeade9a0d
Juan-789 <juan.marulanda@stu.ocsb.ca>
2024-03-13 20:29:23 -0400

    polished the formatting and used the existing logic

--- tests/parsing/test_full_telemetry_parsing.py
+++ tests/parsing/test_full_telemetry_parsing.py
@@ -1,5 +1,3 @@
-import logging
-
 import pytest
 from modules.telemetry.v1.block import BlockHeader
 from modules.telemetry.telemetry_utils import parse_radio_block
@@ -23,11 +21,7 @@ def block_header() -> BlockHeader:
     returns a blockheader
     """
 
-    block_length = int("0x" + LINE1[32:34], 16)
-    block_type = int("0x" + LINE1[34:36], 16)
-    subtype = int("0x" + LINE1[36:38], 16)
-    address = int("0x" + LINE1[38:40], 16)
-    return BlockHeader(block_length, block_type, subtype, address)
+    return BlockHeader.from_hex(LINE1[32:40])
 
 
 @pytest.fixture
@@ -43,21 +37,23 @@ def block_header_error() -> BlockHeader:
     """
     Invalid data block subtype, random non-existant
     """
-    block_length = int("0x" + LINE1[32:34], 16)
-    block_type = int("0x" + LINE1[34:36], 16)
-    subtype = int("0x" + "9A", 16)
-    address = int("0x" + LINE1[38:40], 16)
+
+    bad_header = BlockHeader.from_hex(LINE1[32:40])
+    bad_header.message_subtype = int("0x" + "9A", 16)
     # cannot reach to the NotImplementedError
-    return BlockHeader(block_length, block_type, subtype, address)
+    return bad_header
 
 
 def test_radio_block(pkt_version: int, block_header: BlockHeader, hex_block_contents:str) -> None:
+    """
+    test a proper line on parse_radio_block
+    """
     prb = parse_radio_block(1, block_header, hex_block_contents)
-    assert prb.block_header.length == 2
+    assert prb.block_header.length == 12
     assert prb.block_header.message_type == 0
     assert prb.block_header.message_subtype == 2
     assert prb.block_header.destination == 0
-    assert prb.block_header.valid == True
+    assert prb.block_header.valid is True
     assert prb.block_name == 'temperature'
     assert prb.block_contents.mission_time == 0
 
