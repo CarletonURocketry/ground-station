8ff86d3d7830b06b6b634a5ca4167cb04b87c6a4
Elias Hawa <elias.hawa2468@gmail.com>
2024-03-23 18:42:21 -0400

    Fix flake8

--- tests/parsing/test_full_telemetry_parsing.py
+++ tests/parsing/test_full_telemetry_parsing.py
@@ -9,49 +9,49 @@ config = load_config("config.json")
 # Fixtures
 
 
-# @pytest.fixture
-# def approved_callsigns() -> dict[str, str]:
-#     return config.approved_callsigns
+@pytest.fixture
+def approved_callsigns() -> dict[str, str]:
+    return config.approved_callsigns
 
 
-# @pytest.fixture
-# def valid_packet_header() -> PacketHeader:
-#     # first 32 characters of a packet
-#     hdr = "564133494e490000000c010137000000"
-#     pkt_hdr = PacketHeader.from_hex(hdr)
-#     return pkt_hdr
+@pytest.fixture
+def valid_packet_header() -> PacketHeader:
+    # first 32 characters of a packet
+    hdr = "564133494e490000000c010137000000"
+    pkt_hdr = PacketHeader.from_hex(hdr)
+    return pkt_hdr
 
 
-# @pytest.fixture
-# def non_approved_callsign() -> PacketHeader:
-#     hdr = "52415454204D4F53530c010137000000"
-#     pkt_hdr = PacketHeader.from_hex(hdr)
-#     return pkt_hdr
+@pytest.fixture
+def non_approved_callsign() -> PacketHeader:
+    hdr = "52415454204D4F53530c010137000000"
+    pkt_hdr = PacketHeader.from_hex(hdr)
+    return pkt_hdr
 
 
-# @pytest.fixture
-# def version_num_zero() -> PacketHeader:
-#     hdr = "564133494e490000000c000137000000"
-#     pkt_hdr = PacketHeader.from_hex(hdr)
-#     return pkt_hdr
+@pytest.fixture
+def version_num_zero() -> PacketHeader:
+    hdr = "564133494e490000000c000137000000"
+    pkt_hdr = PacketHeader.from_hex(hdr)
+    return pkt_hdr
 
 
-# @pytest.fixture
-# def invalid_packet_header() -> PacketHeader:
-#     hdr = "52415454204D4F53530c0b0137000000"
-#     pkt_hdr = PacketHeader.from_hex(hdr)
-#     return pkt_hdr
+@pytest.fixture
+def invalid_packet_header() -> PacketHeader:
+    hdr = "52415454204D4F53530c0b0137000000"
+    pkt_hdr = PacketHeader.from_hex(hdr)
+    return pkt_hdr
 
 
-# # Tests
+# Tests
 
 
-# # Test valid header
-# def test_is_valid_hdr(valid_packet_header: PacketHeader, approved_callsigns: dict[str, str]) -> None:
-#     assert is_valid_packet_header(valid_packet_header, approved_callsigns)
+# Test valid header
+def test_is_valid_hdr(valid_packet_header: PacketHeader, approved_callsigns: dict[str, str]) -> None:
+    assert is_valid_packet_header(valid_packet_header, approved_callsigns)
 
 
-# # Test a invalid header: unapproved call sign
+# Test a invalid header: unapproved call sign
 # def test_is_invalid_hdr1(non_approved_callsign: PacketHeader, approved_callsigns: dict[str, str]) -> None:
 #     assert not (is_valid_packet_header(non_approved_callsign, approved_callsigns))
 
