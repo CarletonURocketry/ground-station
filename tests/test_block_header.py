7bbdd0119365de0c58dc3cd27d13ccf428c925a5
AshwinSri23 <ashwin.srib@gmail.com>
2025-07-22 21:12:49 -0400

    Updated block_header tests

--- tests/test_block_header.py
+++ tests/test_block_header.py
@@ -17,7 +17,7 @@ def header1() -> str:
     message sub type: 1
     destination address: 0
     """
-    return "02000100"
+    return "02"
 
 
 @pytest.fixture
@@ -29,7 +29,7 @@ def header2() -> str:
     message sub type: 2
     destination address: 0
     """
-    return "02000200"
+    return "00"
 
 
 @pytest.fixture
@@ -41,7 +41,7 @@ def header3() -> str:
     message sub type: 3
     destination address: 0
     """
-    return "02000300"
+    return "03"
 
 
 @pytest.fixture
@@ -53,11 +53,11 @@ def header1_invalid_message_type() -> str:
     message sub type: 1
     destination address: 0
     """
-    return "02fe0100"
+    return "fe"
 
 
 @pytest.fixture
-def header1_invalid_message_subtype() -> str:
+def header1_too_large() -> str:
     """
     Data block header with the following attributes:
     length: 12 bytes
@@ -80,42 +80,50 @@ def header1_invalid_destination() -> str:
     return "02000105"
 
 
-# def test_parsing_header1(header1: str):
-    #"""Ensure that parsing a block header works as expected."""
-    # hdr = BlockHeader.from_hex(header1)
+def test_parsing_header1(header1: str):
+    """Ensure that parsing a block header works as expected."""
+    header_data = bytes.fromhex(header1)
+    hdr = headers.parse_block_header(header_data)
 
-    # assert len(hdr) == 12
-    # assert hdr.message_type == 0
-    # assert hdr.message_subtype == 1
-    #assert hdr.destination == 0
+    assert len(header_data) == 1
+    assert hdr.type == headers.BlockType.TEMPERATURE
+    
+def test_parsing_header2(header2: str):
+    """Ensure that parsing a block header works as expected."""
+    header_data = bytes.fromhex(header2)
+    hdr = headers.parse_block_header(header_data)
 
-
-# def test_parsing_header2(header2: str):
-#     """Ensure that parsing a block header works as expected."""
-#     hdr = BlockHeader.from_hex(header2)
-
-#     assert len(hdr) == 12
-#     assert hdr.message_type == 0
-#     assert hdr.message_subtype == 2
-#     assert hdr.destination == 0
+    assert len(header_data) == 1
+    assert hdr.type == headers.BlockType.ALTITUDE_ABOVE_SEA_LEVEL
 
 
-# def test_parsing_header3(header3: str):
-#     """Ensure that parsing a block header works as expected."""
-#     hdr = BlockHeader.from_hex(header3)
 
-#     assert len(hdr) == 12
-#     assert hdr.message_type == 0
-#     assert hdr.message_subtype == 3
-#     assert hdr.destination == 0
+def test_parsing_header3(header3: str):
+    """Ensure that parsing a block header works as expected."""
+    header_data = bytes.fromhex(header3)
+    hdr = headers.parse_block_header(header_data)
+
+    assert len(header_data) == 1
+    assert hdr.type == headers.BlockType.PRESSURE
 
 
-# def test_parsing_header1_invalid_message_type(header1_invalid_message_type: str):
-#     """Ensure that parsing a block header with an invalid message type raises an error."""
-#     with pytest.raises(
-#         InvalidHeaderFieldValueError, match="Invalid BlockHeader field: 254 is not a valid value for BlockType"
-#     ):
-#         _ = BlockHeader.from_hex(header1_invalid_message_type)
+def test_parsing_header1_invalid_message_type(header1_invalid_message_type: str):
+    """Ensure that parsing a block header with an invalid message type raises an error."""
+    with pytest.raises(
+        headers.InvalidHeaderFieldValueError, match="Invalid BlockHeader field: fe is not a valid value for bad block header: 254 is not a valid BlockType"
+    ):
+        header_data = bytes.fromhex(header1_invalid_message_type)
+        header = headers.parse_block_header(header_data)
+
+def test_parsing_header1_too_large_header(header1_too_large: str):
+    """Ensure that parsing a block header with an invalid message subtype raises an error."""
+    with pytest.raises(
+        headers.InvalidHeaderFieldValueError,
+        match="Invalid BlockHeader field: 0200fe00 is not a valid value for bad block header: unpack requires a buffer of 1 bytes",
+    ):
+        header_data = bytes.fromhex(header1_too_large)
+        header = headers.parse_block_header(header_data)
+
 
 
 # def test_parsing_header1_invalid_message_subtype(header1_invalid_message_subtype: str):
